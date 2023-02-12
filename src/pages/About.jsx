da2226b97390af7bd72fecd0abb796c9d1d778ea
Lelianto Eko Pradana <lelianto.eko@gmail.com>
2023-02-12 13:35:54 +0700

    routing example

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 const About = () => {
+  const navigate = useNavigate();
+  const goToHome = () => {
+    navigate("/");
+  }
   return (
     <div>
       <div className="flex items-center p-10">
         <h1 className="text-2xl">Take me to Home Page</h1>
-        <button className="bg-blue-500 px-4 text-white ml-4 rounded-full">Home</button>
+        <button onClick={goToHome} className="bg-blue-500 px-4 text-white ml-4 rounded-full">Home</button>
       </div>
     </div>
   );
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,15 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
+
 const Home = () => {
+  const navigate = useNavigate();
+  const goToAbout = () => {
+    navigate("/about");
+  }
   return (
     <div className="flex items-center p-10">
       <h1 className="text-2xl">Take me to About Page</h1>
-      <button className="bg-blue-500 px-4 text-white ml-4 rounded-full">About</button>
+      <button onClick={goToAbout} className="bg-blue-500 px-4 text-white ml-4 rounded-full">About</button>
     </div>
   );
 };
